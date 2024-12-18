b1a1f7dda40aaeb4f21f8da3a49623b9bd91fe10
EmptySet <bodnalev@gmail.com>
2024-12-18 15:07:16 +0000

    tarball

--- setup.py
+++ setup.py
@@ -1,36 +1,11 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
-import os
-import glob
+import os, glob
 
-bliss_include_dir = 'include'
-
-bliss_src_dir = 'src'
-
-bliss_source_files = glob.glob(os.path.join(bliss_src_dir, '*.cc'))
+bliss_source_files = glob.glob("src/*.cc")
 
 with open("README.md", "r") as fp:
     long_desc = fp.read()
-
-extensions = [
-    Extension(
-        name="blisspy",
-        sources=["blisspy.pyx"] + bliss_source_files,
-        language="c++",
-        include_dirs=[
-            bliss_include_dir,
-            os.path.join(bliss_include_dir, 'bliss'),
-        ],
-        extra_compile_args=[
-            '-std=c++11',
-            '-O3',
-            '-fPIC',
-        ],
-        define_macros=[
-            ('NDEBUG', None),
-        ],
-    )
-]
 
 setup(
     name="blisspy",
@@ -41,8 +16,19 @@ setup(
     long_description_content_type='text/markdown',
     long_description=long_desc,
     license="GNU",
-    package_data={'': ['*.pxd']},
+    packages=["blisspy"],
+    package_data={"blisspy": ["*.pxd"]},
     include_package_data=True,
-    ext_modules=cythonize(extensions, compiler_directives={"language_level": "3"}),
+    ext_modules=cythonize([
+        Extension(
+            name="blisspy",
+            sources=["blisspy/__init__.pyx"] + bliss_source_files,
+            language="c++",
+            include_dirs=["include"],
+            extra_compile_args=['-std=c++11', '-O3', '-fPIC'],
+            define_macros=[('NDEBUG', None)]
+        )
+    ], compiler_directives={"language_level": "3"}),
     install_requires=["Cython", "cysignals"]
 )
+
